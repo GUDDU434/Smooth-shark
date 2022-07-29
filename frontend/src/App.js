f14793cdde3d2da08a1ab4846cf0f413a1fdef57
goutham934 <akkaladevigoutham@gmail.com>
2022-07-29 11:12:37 +0530

    done some changes in main branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,9 @@
 import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Setting, { Settingpart, Settingthirdpart } from './components/Settingpage/Setting';
-import VideoPlayer from './pages/VideoPlayer';
-import Sidebar from './pages/Sidebar';
-import Notifications from './pages/Notifications';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: '30px 100px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '600px',
-    border: '2px solid black',
-
-    [theme.breakpoints.down('xs')]: {
-      width: '90%',
-    },
-  },
-  image: {
-    marginLeft: '15px',
-  },
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%',
-  },
-}));
-
+import VideoCall from './routes/VideoCall';
 const App = () => {
-  const classes = useStyles();
-
-
-
-  return (
-    <div className={classes.wrapper}>
-     
-      <VideoPlayer />
-      <Sidebar>
-        <Notifications />
-      </Sidebar>
-      <Setting/>
-      <Settingpart/>
-      <Settingthirdpart/>
-    </div>
-  );
+ return (
+  <VideoCall/>
+ )
 };
 
 export default App;
